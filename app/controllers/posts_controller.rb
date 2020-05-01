b0b667624dc08675e8c668edd9e65ee8d7e87d75
Pierre <pierre.de.vreyer@gmail.com>
2020-05-01 21:02:03 +0800

    Initial commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,17 @@
+class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all.order(create_at: :desc)
+  end
+
+  def create
+    Post.new(post_params)
+    redirect_to posts_path
+  end
+
+  private
+
+    def post_params
+      params.require(:post).permit(:body)
+    end
+end
